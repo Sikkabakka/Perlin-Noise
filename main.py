0556f633965b7723645c26cd34a0f24253cccd56
Elias Akseli Impola Gjerde <Elias.gjerde@gmail.com>
2025-02-04 22:43:16 +0100

    FIND ERROR

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import pygame as pg
-from noise_maker import Noise_maker
+from noise_maker import Noise_maker, Pixel
 
 pg.init()
 
 pixels = 40
 box_size = 1
 grid_size = 10
-screen = pg.display.set_mode((pixels * box_size * grid_size, pixels*box_size *grid_size))
+padding = 40
+screen = pg.display.set_mode((pixels * box_size * grid_size + padding, pixels*box_size *grid_size + padding))
 
 running = True
+
+draw_arrows = False
 
 noise_maker = Noise_maker(grid_size, pixels)
 map = noise_maker.make_map()
 # noise_maker.check_grid()
 def draw_arrow(direction, position, size =1):
-    pg.draw.rect(screen, "red", ())
+
+    pg.draw.line(screen, (255, 0, 0), (position[0]* pixels + padding/2, position[1]*pixels + padding/2), (position[0]* pixels + padding/2 + direction[0]*10, position[1]*pixels + padding/2+direction[1]*10), 3)
+
 
 while running:
     for event in pg.event.get():    
@@ -25,19 +30,34 @@ while running:
 
     for y, row in enumerate(map):
         for x, tile in enumerate(row):
+
             for i, pixel_row in enumerate(tile):
                 for j, pixel in enumerate(pixel_row):
-                    pixel += 1
-                    if pixel > 2:
-                        pixel = 2
-                    elif pixel < 0:
-                        pixel = 0 
-                    color_value = round(255 / 2 * pixel)
 
-                    screen.set_at((x * pixels + j, y * pixels + i), (color_value, color_value, color_value))
+                    pixel_value = pixel.value
+                    pixel_value += 1
+                    if pixel_value > 2:
+                        pixel_value = 2
+                    elif pixel_value < 0:
+                        pixel_value = 0
+                    
 
+                    # if pixel.pixel_count[0] == 39 and pixel.pixel_count[1] == 1:
+                    #     print(pixel.value, "top_right", x, y)
+                    #     print(pixel.corners, "Parents")
+                    # if pixel.pixel_count[0] == 1 and pixel.pixel_count[1] == 1:
+                    #     print(pixel.value, "top_left", x, y)
+                    #     print(pixel.corners, "Parents")
+
+
+
+                    color_value = round(255 / 2 * pixel_value)
+                    screen.set_at((x * pixels+ padding//2 + j , y * pixels + padding//2 + i), (color_value, color_value, color_value))
+    if draw_arrows:
             
-
+        for y, row in enumerate(noise_maker.grid_vectors):
+            for x, direction in enumerate(row):
+                draw_arrow(direction, (x, y))
 
 
     pg.display.update()    
--- noise_maker.py
+++ noise_maker.py
@@ -3,11 +3,28 @@ from random import uniform, randint
 import os
 import math
 
+class Pixel():
+    def __init__(self):
+        self.pixel_count = None
+        self.position = None
+        self.value = None
+        self.corners = None
+        
 
 class Noise_maker():
     def __init__(self, grid_size, pixels):
         self.grid_size = grid_size
         self.pixels = pixels
+        
+        self.vectors = [ np.array([1, 1]), 
+                        np.array([-1, 1]),
+                        np.array([1, -1]),
+                        np.array([-1, -1]),
+                        np.array([np.sqrt(2), 0]),
+                        np.array([0, np.sqrt(2)]),
+                        np.array([-np.sqrt(2), 0]),
+                        np.array([0, -np.sqrt(2)])
+        ]
         self.grid_vectors = self.make_grid()
 
     def check_grid(self):
@@ -19,16 +36,19 @@ class Noise_maker():
     def rand_vector(self):
         x = uniform(0, math.pi * 2)
         return np.array([np.cos(x), np.sin(x)])
+        # index = randint(0, len(self.vectors)-1)
+        # return self.vectors[index]
     
     def make_grid(self):
         return [[self.rand_vector() for j in range(self.grid_size +1)] for i in range(self.grid_size +1)]
 
     def find_corners(self, position):
-        top_left = np.array([math.floor(position[0]), math.floor(position[1])])
+        top_left = np.array([math.floor(position[0]), math.floor(position[1])]) 
         return [top_left, top_left + np.array([1, 0]), top_left + np.array([0, 1]), top_left + np.array([1, 1])]
 
     def lerp(self, x, y, t):
         return x + t*(y - x)
+    
     def smoothstep(self, t):
         t = max(0, min(1, t))
         return t * t * (3 - 2 * t)
@@ -36,17 +56,19 @@ class Noise_maker():
     def find_value(self, position):
         corners = self.find_corners(position)
 
-        corner_vectors = [position -corner for corner in corners]
-        corner_normal_vectors = [self.grid_vectors[corner[1]][corner[0]] for corner in corners]
+        corner_vectors = [position -corner for corner in corners] # riktig
+        corner_normal_vectors = [self.grid_vectors[int(corner[0])][int(corner[1])] for corner in corners] #riktig
 
         dot_products = [np.dot(corner_normal_vectors[i], corner_vectors[i]) for i in range(4)]
        
         # linear interpolation with x and y values as weights
-        x_weight = self.smoothstep(position[0] - corners[0][0])
-        l_interpolation = self.lerp(dot_products[0]*-1, dot_products[2]*-1, x_weight)
-        s_interpolation = self.lerp(dot_products[1]*-1, dot_products[3]*-1, x_weight)
+        # x_weight = self.smoothstep(position[0] - corners[0][0])
+        x_weight = position[0] - corners[0][0]
+        l_interpolation = self.lerp(dot_products[0], dot_products[1], x_weight)
+        s_interpolation = self.lerp(dot_products[2], dot_products[3], x_weight)
 
-        y_weight = self.smoothstep(position[1]-corners[0][1])
+        # y_weight = self.smoothstep(position[1]-corners[0][1])
+        y_weight = position[1]-corners[0][1]
         y_interpolation =self.lerp(l_interpolation, s_interpolation, y_weight)
         # if y_interpolation > 1:
         #     print("bigger then 1")
@@ -56,22 +78,24 @@ class Noise_maker():
         #     print(y_interpolation, "y_inte")
         return y_interpolation
     def find_pixels(self, grid_index):
-        value = 0
+        
         map = []
         for i in range(self.pixels):
                 temp = []
                 for j in range(self.pixels):
-                    value = self.find_value(np.array([grid_index[0] + 1/self.pixels * j, grid_index[1]+ 1/self.pixels *i])) 
-                    temp.append(value)
+                    pixel = Pixel()
+                    pixel.pixel_count = (j, i)
+                    pixel.value = self.find_value(np.array([grid_index[0] + 1/self.pixels * j, grid_index[1]+ 1/self.pixels *i])) 
+                    pixel.position = grid_index[0] + 1/self.pixels * j, grid_index[1]+ 1/self.pixels *i
+                    pixel.corners = self.find_corners(pixel.position)
+                    temp.append(pixel)
                 map.append(temp)
         return map
-    def make_map(self):
-        
+    def make_map(self): 
         map = []
-        for i in range(self.grid_size ):
+        for i in range(self.grid_size):
             temp = []
             for j in range(self.grid_size):
-    
                 temp.append(self.find_pixels((j, i)))
             map.append(temp)
             
